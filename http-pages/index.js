008ec5e782e74230fcc84503ff8badce8ee15ecb
Ilya Nikalayeu <Ilya_Nikalayeu@epam.com>
2018-02-01 12:58:32 +0300

    add js for addUser page

--- http-pages/index.js
+++ http-pages/index.js
@@ -1,3 +1,10 @@
-function getValue(obj) {
-    alert(obj.value);
+function toggle() {
+    console.log('tyt');
+    var div = document.getElementById('customCheck2');
+    if (document.getElementById('roles').style.display == 'block') {
+        document.getElementById('roles').style.display = 'none';
+    } else {
+        document.getElementById('roles').style.display = 'block';
+    }
 }
+
