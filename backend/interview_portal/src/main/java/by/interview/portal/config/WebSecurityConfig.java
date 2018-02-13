084cf9d98818d667d0bebc2f9492942f2329cc74
Yauheni_Verameichyk <yauheni_verameichyk@epam.com>
2018-02-13 13:59:21 +0300

    Minor improvements

--- backend/interview_portal/src/main/java/by/interview/portal/config/WebSecurityConfig.java
+++ backend/interview_portal/src/main/java/by/interview/portal/config/WebSecurityConfig.java
@@ -1,12 +1,12 @@
 package by.interview.portal.config;
 
-import by.interview.portal.filter.JwtAuthenticationTokenFilter;
-import by.interview.portal.security.JwtAuthenticationEntryPoint;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -14,62 +14,52 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import by.interview.portal.filter.JwtAuthenticationTokenFilter;
+import by.interview.portal.security.JwtAuthenticationEntryPoint;
+
+@Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+	@Autowired
+	private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 
-    @Autowired
-    private UserDetailsService userDetailsService;
+	@Autowired
+	private UserDetailsService userDetailsService;
 
-    @Autowired
-    public void configureAuthentication(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception{
-        authenticationManagerBuilder.userDetailsService(this.userDetailsService)
-                .passwordEncoder(passwordEncoder());
-    }
+	@Autowired
+	public void configureAuthentication(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+		authenticationManagerBuilder.userDetailsService(this.userDetailsService).passwordEncoder(passwordEncoder());
+	}
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-    @Bean
-    public JwtAuthenticationTokenFilter authenticationTokenFilterBean() throws Exception {
-        return new JwtAuthenticationTokenFilter();
-    }
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .exceptionHandling()
-                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
-                .and()
-                //don't create session
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authorizeRequests()
-                // allow anonymous resource requests
-                .antMatchers(
-                        HttpMethod.GET,
-                        "/",
-                        "/*.html",
-                        "/favicon.ico",
-                        "/**/*.html",
-                        "/**/*.css",
-                        "/**/*.js"
-                ).permitAll()
-                // disable secure H2 Database
-                .antMatchers("/h2-console/**/**").permitAll()
+	@Bean
+	public JwtAuthenticationTokenFilter authenticationTokenFilterBean() throws Exception {
+		return new JwtAuthenticationTokenFilter();
+	}
 
-                .antMatchers("/auth/**").permitAll()
-                .anyRequest().authenticated();
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and()
+				// don't create session
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequests()
+				// allow anonymous resource requests
+				.antMatchers(HttpMethod.GET, "/", "/*.html", "/favicon.ico", "/**/*.html", "/**/*.css", "/**/*.js")
+				.permitAll()
+				// disable secure H2 Database
+				.antMatchers("/h2-console/**/**").permitAll()
 
-        // Custom JWT based security filter
-        http.addFilterBefore(authenticationTokenFilterBean(), UsernamePasswordAuthenticationFilter.class);
+				.antMatchers("/auth/**").permitAll().anyRequest().authenticated();
 
-        // disable page caching
-        http.headers()
-                .frameOptions().sameOrigin()  // required to set for H2 else H2 Console will be blank.
-                .cacheControl();
-    }
+		// Custom JWT based security filter
+		http.addFilterBefore(authenticationTokenFilterBean(), UsernamePasswordAuthenticationFilter.class);
+
+		// disable page caching
+		http.headers().frameOptions().sameOrigin() // required to set for H2 else H2 Console will be blank.
+				.cacheControl();
+	}
 }
--- backend/interview_portal/src/main/java/by/interview/portal/filter/JwtAuthenticationTokenFilter.java
+++ backend/interview_portal/src/main/java/by/interview/portal/filter/JwtAuthenticationTokenFilter.java
@@ -1,8 +1,12 @@
 package by.interview.portal.filter;
 
-import by.interview.portal.Constant.JwtConstant;
-import by.interview.portal.security.JwtTokenUtil;
-import io.jsonwebtoken.ExpiredJwtException;
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -11,56 +15,58 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import by.interview.portal.Constant.JwtConstant;
+import by.interview.portal.security.JwtTokenUtil;
+import io.jsonwebtoken.ExpiredJwtException;
 
 public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
 
-    @Autowired
-    UserDetailsService userDetailsService;
+	@Autowired
+	UserDetailsService userDetailsService;
 
-    @Autowired
-    private JwtTokenUtil jwtTokenUtil;
-    @Override
-    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
-        String header = httpServletRequest.getHeader(JwtConstant.HEADER_STRING);
-        String login = null;
-        String authToken = null;
-        if(header != null
-                && header.startsWith(JwtConstant.TOKEN_PREFIX)){
-            authToken = header.replace(JwtConstant.TOKEN_PREFIX, "");
+	@Autowired
+	private JwtTokenUtil jwtTokenUtil;
 
-            try{
-                login = jwtTokenUtil.getloginFomToken(authToken);
-            }catch (IllegalArgumentException e) {
-                logger.error("an error occured during getting username from token", e);
-            } catch (ExpiredJwtException e) {
-                logger.warn("the token is expired and not valid anymore", e);
-            }
-        }else {
-            logger.warn("couldn't find bearer string, will ignore the header");
-        }
-        logger.info("checking authentication for user " + login);
-        if (login != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+	@Override
+	protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
+			FilterChain filterChain) throws ServletException, IOException {
+		String header = httpServletRequest.getHeader(JwtConstant.HEADER_STRING);
+		String login = null;
+		String authToken = null;
+		if (header != null && header.startsWith(JwtConstant.TOKEN_PREFIX)) {
+			authToken = header.replace(JwtConstant.TOKEN_PREFIX, "");
 
-            // It is not compelling necessary to load the use details from the database. You could also store the information
-            // in the token and read it from it. It's up to you ;)
-            UserDetails userDetails = this.userDetailsService.loadUserByUsername(login);
+			try {
+				login = jwtTokenUtil.getloginFomToken(authToken);
+			} catch (IllegalArgumentException e) {
+				logger.error("an error occured during getting username from token", e);
+			} catch (ExpiredJwtException e) {
+				logger.warn("the token is expired and not valid anymore", e);
+			}
+		} else {
+			logger.warn("couldn't find bearer string, will ignore the header");
+		}
+		logger.info("checking authentication for user " + login);
+		if (login != null && SecurityContextHolder.getContext().getAuthentication() == null) {
 
-            // For simple validation it is completely sufficient to just check the token integrity. You don't have to call
-            // the database compellingly. Again it's up to you ;)
-            if (jwtTokenUtil.validateToken(authToken, userDetails)) {
-                UsernamePasswordAuthenticationToken authentication =
-                        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(httpServletRequest));
-                logger.info("authenticated user " + login + ", setting security context");
-                SecurityContextHolder.getContext().setAuthentication(authentication);
-            }
-        }
+			// It is not compelling necessary to load the use details from the database. You
+			// could also store the information
+			// in the token and read it from it. It's up to you ;)
+			UserDetails userDetails = this.userDetailsService.loadUserByUsername(login);
 
-        filterChain.doFilter(httpServletRequest, httpServletResponse);
-    }
+			// For simple validation it is completely sufficient to just check the token
+			// integrity. You don't have to call
+			// the database compellingly. Again it's up to you ;)
+
+			if (jwtTokenUtil.validateToken(authToken, userDetails)) {
+				UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+						userDetails, null, userDetails.getAuthorities());
+				authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(httpServletRequest));
+				logger.info("authenticated user " + login + ", setting security context");
+				SecurityContextHolder.getContext().setAuthentication(authentication);
+			}
+		}
+
+		filterChain.doFilter(httpServletRequest, httpServletResponse);
+	}
 }
