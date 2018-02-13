f37d2e922c7276ff281aefdf539b66e46df89dba
Ilya Nikalayeu <Ilya_Nikalayeu@epam.com>
2018-02-13 13:41:02 +0300

    add JwtAuthenticationTokenFilter

--- backend/interview_portal/src/main/java/by/interview/portal/Constant/JwtConstant.java
+++ backend/interview_portal/src/main/java/by/interview/portal/Constant/JwtConstant.java
@@ -3,4 +3,5 @@ package by.interview.portal.Constant;
 public class JwtConstant {
     public static final String HEADER_STRING = "Authorization";
     public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String SIGNING_KEY = "viktar";
 }
--- backend/interview_portal/src/main/java/by/interview/portal/filter/JwtAuthenticationTokenFilter.java
+++ backend/interview_portal/src/main/java/by/interview/portal/filter/JwtAuthenticationTokenFilter.java
@@ -2,8 +2,13 @@ package by.interview.portal.filter;
 
 import by.interview.portal.Constant.JwtConstant;
 import by.interview.portal.security.JwtTokenUtil;
+import io.jsonwebtoken.ExpiredJwtException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -24,10 +29,38 @@ public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
         String header = httpServletRequest.getHeader(JwtConstant.HEADER_STRING);
         String login = null;
         String authToken = null;
-        if(header != null && header.startsWith(JwtConstant.TOKEN_PREFIX)){
+        if(header != null
+                && header.startsWith(JwtConstant.TOKEN_PREFIX)){
             authToken = header.replace(JwtConstant.TOKEN_PREFIX, "");
+
+            try{
+                login = jwtTokenUtil.getloginFomToken(authToken);
+            }catch (IllegalArgumentException e) {
+                logger.error("an error occured during getting username from token", e);
+            } catch (ExpiredJwtException e) {
+                logger.warn("the token is expired and not valid anymore", e);
+            }
+        }else {
+            logger.warn("couldn't find bearer string, will ignore the header");
+        }
+        logger.info("checking authentication for user " + login);
+        if (login != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+
+            // It is not compelling necessary to load the use details from the database. You could also store the information
+            // in the token and read it from it. It's up to you ;)
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(login);
+
+            // For simple validation it is completely sufficient to just check the token integrity. You don't have to call
+            // the database compellingly. Again it's up to you ;)
+            if (jwtTokenUtil.validateToken(authToken, userDetails)) {
+                UsernamePasswordAuthenticationToken authentication =
+                        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(httpServletRequest));
+                logger.info("authenticated user " + login + ", setting security context");
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
         }
 
-
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
     }
 }
