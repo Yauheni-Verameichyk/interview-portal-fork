93de959fee41ba874c3e1c7d524a707c59313b51
Viktar <Viktar_Hrynko@epam.com>
2018-02-14 15:53:24 +0300

    create author. page

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
 import { MenuComponent } from './component/menu/menu.component';
+import { LoginComponent } from './component/login/login.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MenuComponent
+    MenuComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
