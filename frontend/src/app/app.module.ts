bfa9577caeac971ad8d14a4e859ae58b4abd1a2a
Yauheni_Verameichyk <yauheni_verameichyk@epam.com>
2018-02-16 10:56:05 +0300

    Added login form validation

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -12,6 +12,7 @@ import { UserPageComponent } from './component/user-page/user-page.component';
 import { DisciplinePageComponent } from './component/discipline-page/discipline-page.component';
 import { ApiModule } from './api/api.module';
 import { AuthenticationControllerService, UserControllerService } from './api/services';
+
 
 @NgModule({
   declarations: [
@@ -25,11 +26,12 @@ import { AuthenticationControllerService, UserControllerService } from './api/se
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule,
     ApiModule
   ],
   providers: [AuthenticationControllerService,
-              UserControllerService],
+    UserControllerService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- frontend/src/app/component/login/login.component.ts
+++ frontend/src/app/component/login/login.component.ts
@@ -1,37 +1,62 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthenticationControllerService } from '../../api/services';
 import { AuthenticationDTO } from '../../api/models';
 import { HttpResponse } from 'selenium-webdriver/http';
+import { FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
   @Output() onChanged = new EventEmitter();
-  user: AuthenticationDTO = {
-    login: "",
-    password: ""
-  }
-  errorMessage: string = "";
-  
-  constructor(private router: Router,
-              private authenticationService: AuthenticationControllerService) {}
+  public user: AuthenticationDTO = {
+    login: '',
+    password: ''
+  };
+  errorMessage: string;
 
-  change(){
+  userForm: FormGroup;
+
+  constructor(private router: Router,
+    private authenticationService: AuthenticationControllerService) { }
+
+  ngOnInit() {
+    this.userForm = new FormGroup({
+      'login': new FormControl(this.user.login, [
+        Validators.required,
+        Validators.minLength(4),
+      ]),
+      'password': new FormControl(this.user.password, [
+        Validators.required,
+        Validators.minLength(2),
+      ])
+    });
+  }
+
+  authorize(): void {
+    this.initializeUser(this.userForm.get('login').value, this.userForm.get('password').value);
     this.authenticationService
       .authorizationUsingPOST(this.user)
       .subscribe((body: string) => {
-        localStorage.setItem("token", body);
+        localStorage.setItem('token', body);
         this.onChanged.emit();
         this.router.navigate(['user']);
-        }, (error: any) => {
-          localStorage.removeItem("token");
-          this.errorMessage = "login or password wrong"
-        } );
+      }, (error: any) => {
+        localStorage.removeItem('token');
+        this.errorMessage = 'wrong login or password';
+      });
   }
 
+  initializeUser(login: string, password: string): void {
+    this.user.login = login;
+    this.user.password = password;
+  }
+
+  get login() { return this.userForm.get('login'); }
+
+  get password() { return this.userForm.get('password'); }
 }
