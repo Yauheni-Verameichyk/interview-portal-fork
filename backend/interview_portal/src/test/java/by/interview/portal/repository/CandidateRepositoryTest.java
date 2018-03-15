abfe65e777f2a42fe6165e4114e51e346ed2309b
Viktor Grinko <fevor@tut.by>
2018-03-15 10:31:37 +0300

    Update CandidateRepositoryTest.java

--- backend/interview_portal/src/test/java/by/interview/portal/repository/CandidateRepositoryTest.java
+++ backend/interview_portal/src/test/java/by/interview/portal/repository/CandidateRepositoryTest.java
@@ -1,7 +1,9 @@
 package by.interview.portal.repository;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.EntityManager;
 
