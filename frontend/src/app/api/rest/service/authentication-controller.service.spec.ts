dbbb109627350354aa55e86f3ae8f9f16aec649c
Yauheni-Verameichyk <36076334+Yauheni-Verameichyk@users.noreply.github.com>
2018-03-20 14:24:57 +0300

    Fixed some tests (#37)
    * Added view discipline page. CreateDisciplineComponent renamed to DisciplineFormComponent
    
    * Added tests for PermissionController, UserController, DisciplineDTOConverter, DisciplineWithHeadsConverter
    
    * Format changes
    
    * Rewritten AuthenticationControllerTest
    
    * EqualsAndHashCode annotation removed
    
    * Added tests for disciplines backend
    
    * Minor improvements
    
    * Very minor improvements
    
    * Relocated DisciplineServiceTest
    
    * Added unfinished discipline-form.component.spec.ts
    
    * Finished tests for DisciplineFormComponent
    
    * Added couple use cases for DisciplineFormComponent test
    
    * Added test for DisciplineComponent
    
    * Added tests for DisciplinesListComponent
    
    * Added tests for SelectUserComponent
    
    * Minor improvements
    
    * Fixed some tests
    
    * Minor improvements

--- frontend/src/app/api/rest/service/authentication-controller.service.spec.ts
+++ frontend/src/app/api/rest/service/authentication-controller.service.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { AuthenticationControllerService } from './authentication-controller.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('AuthenticationControllerService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientModule],
       providers: [AuthenticationControllerService]
     });
   });
