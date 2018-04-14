a32da6131e1afef525f2c3b93b5311f3710426c1
92nikolaev <92nikolaev@gmail.com>
2018-04-14 20:47:21 +0300

    Updated tests (#63)
    * Implemented create, update, delete users. Assign, update, delete discipline to users. Assign, update, delete roles to users. Update common styles to the application. When scrolling users list autoload. Created page for 404 error.
    
    * update style ui
    
    * Updated: tests for candidates, file 'backup.sql', users form(popUp), navigation(underline and lighting) you can see where you are!
    
    * add test for page not found component and navbar update beckup sql
    
    * added tests
    
    * Completely covered with tests AuthenticationService
    
    * Added tests for navbar-manager and user-list.component
    
    * updated tests
    
    * updated spec files

--- frontend/src/app/interviews/interview/interview.component.spec.ts
+++ frontend/src/app/interviews/interview/interview.component.spec.ts
@@ -1,21 +1,45 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { InterviewComponent } from './interview.component';
-
+import { Router, NavigationExtras, RouterModule, ActivatedRoute } from '@angular/router';
+import { InterviewService } from '../service/interview.service';
+const routerStub = {
+  navigate(commands: any[], extras?: NavigationExtras) { },
+  navigateByUrl(url: string) { return url; }
+};
+const interviewServiceStub = {
+  removeInterview(id: number) { }
+};
 describe('InterviewComponent', () => {
   let component: InterviewComponent;
   let fixture: ComponentFixture<InterviewComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ InterviewComponent ]
+      declarations: [InterviewComponent],
+      providers: [
+        { provide: Router, useValue: routerStub },
+        { provide: InterviewService, useValue: interviewServiceStub },
+        { provide: ActivatedRoute, useValue: interviewServiceStub },
+      ],
+      imports: [ RouterModule ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(InterviewComponent);
     component = fixture.componentInstance;
+    component.interview = {
+      'id': 2, 'startTime': '2018-04-13T15:00:00',
+      'endTime': '2018-04-13T17:00:00',
+      'candidate': { 'id': 41, 'name': 'Cherly', 'surname': 'Jones' },
+      'place': 'B221 K3V', 'status': 'wait',
+      'interviewerSet': [{ 'id': 22, 'name': 'Gary', 'surname': 'Ortiz', 'roles': null }
+        , { 'id': 12, 'name': 'Viktor', 'surname': 'Grinko', 'roles': null }]
+      , 'disciplineSet': [{ 'id': 11, 'name': 'Visual basic' }
+        , { 'id': 1, 'name': 'Java' }]
+    };
     fixture.detectChanges();
   });
 
