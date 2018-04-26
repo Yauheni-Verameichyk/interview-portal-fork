97c7d5f975c42841abd4eb420f5aa820bb56e07a
92nikolaev <92nikolaev@gmail.com>
2018-04-26 15:29:33 +0300

    Implemented debouncer for search users (#72)

--- frontend/src/app/users/user-search/user-search.component.ts
+++ frontend/src/app/users/user-search/user-search.component.ts
@@ -1,15 +1,16 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { EventTargetLike } from 'rxjs/observable/FromEventObservable';
 import { Subject } from 'rxjs/Subject';
-
+import 'rxjs/add/operator/debounceTime';
 @Component({
   selector: 'app-user-search',
   templateUrl: './user-search.component.html',
   styleUrls: ['./user-search.component.css']
 })
-export class UserSearchComponent {
+export class UserSearchComponent implements OnInit {
   @Output() private parameters = new EventEmitter<string>();
+  private expectationTime = new Subject();
   isCoordinator = true;
   isDisciplineHead = true;
   isInterviewer = true;
@@ -17,14 +18,22 @@ export class UserSearchComponent {
   userName = '';
   private keyUp = new Subject<string>();
 
+  ngOnInit(): void {
+    // you listen to values here which are debounced
+    // on every value, you call the outer component
+    this.expectationTime.debounceTime(350).subscribe((value => this.parameters.emit(value.toString())));
+  }
+
   searchByParameters(): void {
     if (this.userName.length > 0) {
       const separator = this.isChosen() ? ';' : '';
       const searchString = `name:${this.userName}${separator}${this.concatParameters()}`;
-      this.parameters.emit(searchString);
+      // send every value from the inner to the subject
+      this.expectationTime.next(searchString);
     } else {
       const searchString = `${this.concatParameters()}`;
-      this.parameters.emit(searchString);
+      // send every value from the inner to the subject
+      this.expectationTime.next(searchString)
     }
 
   }
